54dc772894cef4176d320c7889eb6910a3a2777f
Bershikha <bsbershikharex@gmail.com>
2026-03-24 11:30:31 +0530

    Implement robot bounded movement solution

---
+++ DAY4/L1041.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        int x = 0, y = 0;
+        int dir = 0; // 0=N, 1=E, 2=S, 3=W
+        
+        for (char ch : instructions.toCharArray()) {
+            if (ch == 'G') {
+                if (dir == 0) y++;
+                else if (dir == 1) x++;
+                else if (dir == 2) y--;
+                else x--;
+            } 
+            else if (ch == 'L') {
+                dir = (dir + 3) % 4;
+            } 
+            else { // 'R'
+                dir = (dir + 1) % 4;
+            }
+        }
+        
+        // Key condition
+        return (x == 0 && y == 0) || dir != 0;
+    }
+}
