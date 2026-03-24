7983f14b6a66a526b146826a9b0efc9cd8d7a881
Bershikha <bsbershikharex@gmail.com>
2026-03-24 11:03:06 +0530

    Add merge function to combine two sorted arrays
    Implemented merge function to combine two sorted arrays and sort the result.

---
+++ DAY3/L88.java
@@ -0,0 +1,8 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int j=m,k=0;k<n;j++,k++){
+            nums1[j]=nums2[k];
+        }
+        Arrays.sort(nums1);
+    }
+}
